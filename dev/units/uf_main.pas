791a08ce530e179b0fae782a316becb7dbaa6707
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-31 06:57:26 +0700

    Update uf_main.pas

--- dev/units/uf_main.pas
+++ dev/units/uf_main.pas
@@ -6,25 +6,29 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
-  ExtCtrls, Grids, StdCtrls;
+  ExtCtrls, Grids, StdCtrls, ActnList;
 
 type
 
   { TFormMain }
 
   TFormMain = class(TForm)
+    ActionDataOpen: TAction;
+    ActionListMain: TActionList;
     MainMenuMain: TMainMenu;
+    MenuItemPopupOpenData: TMenuItem;
     MenuItemFileOpenData: TMenuItem;
     MenuItemFile: TMenuItem;
     OpenDialogMain: TOpenDialog;
     PanelTop: TPanel;
     PanelBottom: TPanel;
+    PopupMenuData: TPopupMenu;
     SplitterBottom: TSplitter;
     StatusBar1: TStatusBar;
     ToolBar1: TToolBar;
     ToolButton1: TToolButton;
+    procedure ActionDataOpenExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure MenuItemFileOpenDataClick(Sender: TObject);
   private
 
   public
@@ -43,17 +47,17 @@ implementation
 uses
   uc_main;
 
-procedure TFormMain.MenuItemFileOpenDataClick(Sender: TObject);
-begin
-  if Self.OpenDialogMain.Execute then
-  begin
-    // Self.StringGridMain.LoadFromCSVFile(Self.OpenDialogMain.FileName);
-  end;
-end;
-
 procedure TFormMain.FormCreate(Sender: TObject);
 begin
   vucMain := tucMain.Create(Self,Self.PanelTop,Self.PanelBottom);
+end;
+
+procedure TFormMain.ActionDataOpenExecute(Sender: TObject);
+begin
+  if Self.OpenDialogMain.Execute then
+  begin
+    vucMain.fcOpenData(Self.OpenDialogMain.FileName);
+  end;
 end;
 
 end.
