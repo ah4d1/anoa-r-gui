3abbb0e413bac699786ad543e3c7ce7bb80a22cd
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-31 08:03:39 +0700

    Update uc_main.pas

--- dev/units/uc_main.pas
+++ dev/units/uc_main.pas
@@ -5,18 +5,23 @@ unit uc_main;
 interface
 
 uses
-  Classes, SysUtils, Controls, ExtCtrls, uc_stringgrid, uc_pagecontrol, Dialogs;
+  Classes, SysUtils, Controls, ExtCtrls, uc_stringgrid, uc_pagecontrol, uc_rcommand,
+  Dialogs, Menus;
 
 type
   tucMain = class(TComponent)
   private
     FStringGrid : tucStringGrid;
     FPageControl : tucPageControl;
+    FRCommand : tucRCommand;
   public
     property vStringGrid : tucStringGrid read FStringGrid write FStringGrid;
     property vPageControl : tucPageControl read FPageControl write FPageControl;
-    constructor Create (AOwner : TWinControl; APanelLeft,APanelBottom : TPanel);
+    property vRCommand : tucRCommand read FRCommand write FRCommand;
+    constructor Create (AOwner : TWinControl; APanelLeft,APanelBottom : TPanel;
+      APopupMenu : TPopupMenu);
     procedure fcOpenData (AFileName : TFileName);
+    procedure fcRun (ACommand : targCommand);
   end;
 
 var
@@ -24,18 +29,26 @@ var
 
 implementation
 
-constructor tucMain.Create (AOwner : TWinControl; APanelLeft,APanelBottom : TPanel);
+constructor tucMain.Create (AOwner : TWinControl; APanelLeft,APanelBottom : TPanel;
+  APopupMenu : TPopupMenu);
 begin
   inherited Create(AOwner);
   Self.vStringGrid := tucStringGrid.Create(AOwner);
   Self.vStringGrid.Parent := APanelLeft;
+  Self.vStringGrid.fcInit(APopupMenu);
   Self.vPageControl := tucPageControl.Create(AOwner);
   Self.vPageControl.Parent := APanelBottom;
+  Self.vRCommand := tucRCommand.Create;
 end;
 
 procedure tucMain.fcOpenData (AFileName : TFileName);
 begin
   Self.vStringGrid.fcOpenData(AFileName);
+end;
+
+procedure tucMain.fcRun (ACommand : targCommand);
+begin
+  vucRCommand.fcRun(ACommand);
 end;
 
 end.
